8bd4593ecf66c6b16a76c5ad694394dbd6c9243b
Zsombor Gegesy <gzsombor@gmail.com>
2018-01-09 00:37:09 +0100

    Subscribe to channel and open channel menu items added to the video listing, and only show the 'subscribe' menu item, if the channel is not yet subscribed.

--- app/src/main/java/free/rm/skytube/gui/fragments/YouTubePlayerV1Fragment.java
+++ app/src/main/java/free/rm/skytube/gui/fragments/YouTubePlayerV1Fragment.java
@@ -645,6 +645,13 @@ public class YouTubePlayerV1Fragment extends ImmersiveModeFragment implements Me
 		} else {
 			menu.findItem(R.id.download_video).setVisible(false);
 		}
+		if (youTubeVideo != null && youTubeVideo.getChannelId() != null) {
+			menu.findItem(R.id.subscribe_channel).setVisible(true);
+			menu.findItem(R.id.open_channel).setVisible(true);
+		} else {
+			menu.findItem(R.id.subscribe_channel).setVisible(false);
+			menu.findItem(R.id.open_channel).setVisible(false);
+		}
 	}
 
 	@Override
@@ -659,8 +666,9 @@ public class YouTubePlayerV1Fragment extends ImmersiveModeFragment implements Me
 		//
 		// youTubeVideo might be null if we have only passed the video URL to this fragment (i.e.
 		// the app is still trying to construct youTubeVideo in the background).
-		if (youTubeVideo != null)
+		if (youTubeVideo != null) {
 			new IsVideoBookmarkedTask(youTubeVideo, menu).executeInParallel();
+		}
 	}
 
 
@@ -709,6 +717,14 @@ public class YouTubePlayerV1Fragment extends ImmersiveModeFragment implements Me
 
             case R.id.block_channel:
 	            youTubeChannel.blockChannel();
+	            return true;
+			case R.id.subscribe_channel:
+				youTubeVideo.subscribeChannel(getContext(), menu);
+				return true;
+
+			case R.id.open_channel:
+				youTubeVideo.openChannel(getContext());
+				return true;
 
 			default:
 				return super.onOptionsItemSelected(item);
