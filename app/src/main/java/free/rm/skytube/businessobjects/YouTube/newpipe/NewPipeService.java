0f6847dcba327a67df2123cd37b22a1104087bf5
Zsombor Gegesy <gzsombor@gmail.com>
2019-08-04 23:42:36 +0200

    Log before YoutubeAPI calls, remove System.out logs and extra log...

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/newpipe/NewPipeService.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/newpipe/NewPipeService.java
@@ -310,6 +310,7 @@ public class NewPipeService {
     public YouTubeVideo getDetails(String videoId) throws ExtractionException, IOException {
         LinkHandler url = streamingService.getStreamLHFactory().fromId(videoId);
         StreamExtractor extractor = streamingService.getStreamExtractor(url);
+        Logger.i(this, "getDetails for %s -> url", videoId, url);
         extractor.fetchPage();
 
         DateInfo uploadDate = new DateInfo(extractor.getUploadDate());
