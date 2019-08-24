e7a1b611e5f271e8d50e7139bbf1e8ebcab56d5b
Zsombor Gegesy <gzsombor@gmail.com>
2019-08-24 23:54:11 +0200

    Loggin, logging, logging, and debug tweaks

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/YouTubeVideo.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/YouTubeVideo.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -41,6 +42,7 @@ import org.joda.time.format.PeriodFormatter;
 import java.io.File;
 import java.io.Serializable;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
@@ -488,8 +490,10 @@ public class YouTubeVideo extends CardData implements Serializable {
 	 */
 	public void removeDownload() {
 		Uri uri = DownloadedVideosDb.getVideoDownloadsDb().getVideoFileUri(YouTubeVideo.this);
+		Log.i("YouTubeVideo", "removeDownload for " + id + " : " + title + " -> " + uri);
 		File file = new File(uri.getPath());
 		if (file.exists()) {
+			Log.i("YouTubeVideo", "File exists " + file.getAbsolutePath());
 			file.delete();
 		}
 		if (SkyTubeApp.getSettings().isDownloadToSeparateFolders()) {
@@ -500,15 +504,19 @@ public class YouTubeVideo extends CardData implements Serializable {
 
 	private void removeParentFolderIfEmpty(File file) {
 		File parentFile = file.getParentFile();
+		Log.i("YouTubeVideo", "removeParentFolderIfEmpty " + file.getAbsolutePath() + " -> " + parentFile.getAbsolutePath() + " "+parentFile.exists() + " "+ parentFile.isDirectory());
 		if (parentFile.exists() && parentFile.isDirectory()) {
 			String[] fileList = parentFile.list();
+			Log.i("YouTubeVideo", "file list is " + Arrays.asList(fileList));
 			if (fileList != null) {
 				if (fileList.length == 0) {
 					// that was the last file in the directory, remove it
+					Log.i("YouTubeVideo", "now delete it:" + parentFile);
 					parentFile.delete();
 				}
 			}
 		}
+		Log.i("YouTubeVideo", "exit removeParentFolderIfEmpty");
 	}
 
 
