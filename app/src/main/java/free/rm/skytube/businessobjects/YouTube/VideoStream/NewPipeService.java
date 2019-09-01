7d7afdcbc6a40e2fa5e030f0c47e96d2e0d08f29
Zsombor Gegesy <gzsombor@gmail.com>
2019-09-02 00:23:22 +0200

    Loggin, logging, logging...

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/VideoStream/NewPipeService.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/VideoStream/NewPipeService.java
@@ -192,7 +192,7 @@ public class NewPipeService {
     public YouTubeVideo getDetails(String videoId) throws ExtractionException, IOException {
         LinkHandler url = streamingService.getStreamLHFactory().fromId(videoId);
         StreamExtractor extractor = streamingService.getStreamExtractor(url);
-        Logger.i(this, "getDetails for %s -> %s", videoId, url);
+        Logger.i(this, "getDetails for %s -> %s", videoId, url.getUrl());
         extractor.fetchPage();
 
         String dateStr = extractor.getUploadDate();
@@ -248,7 +248,7 @@ public class NewPipeService {
     public Pager<InfoItem> getSearchResult(String query) throws ExtractionException, IOException {
         SearchExtractor extractor = streamingService.getSearchExtractor(query);
         extractor.fetchPage();
-
+        Logger.i(this, "getSearchResult for %s -> %s", query, extractor.getOriginalUrl());
         return new Pager<>(streamingService, extractor, null);
     }
 
