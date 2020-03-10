b455f75adf1d555ccbf4283b6ddf2321efe3c0bb
Zsombor Gegesy <gzsombor@gmail.com>
2020-03-10 23:11:35 +0100

    Try to save into external SD card

--- app/src/main/java/free/rm/skytube/app/Settings.java
+++ app/src/main/java/free/rm/skytube/app/Settings.java
@@ -120,6 +120,10 @@ public class Settings {
         return !getPreference(R.string.pref_key_disable_playback_status, false);
     }
 
+    public boolean isDownloadToExternalStorage() {
+        return getSharedPreferences().getBoolean(app.getStr(R.string.pref_key_download_to_external_storage),false);
+    }
+
     public boolean isDownloadToTemporaryFolder() {
         return getSharedPreferences().getBoolean(app.getStr(R.string.pref_key_download_to_temporary_directory),false);
     }
@@ -266,7 +270,11 @@ public class Settings {
     }
 
     public String getDownloadFolder(String defaultValue) {
-        return getPreference(R.string.pref_key_video_download_folder, defaultValue);
+        if (isDownloadToExternalStorage()) {
+            return null;
+        } else {
+            return getPreference(R.string.pref_key_video_download_folder, defaultValue);
+        }
     }
 
     public File getDownloadParentFolder() {
