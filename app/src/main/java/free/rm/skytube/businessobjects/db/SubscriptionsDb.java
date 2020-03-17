931a4e4b214cd892d0d954565ff5890f3a7d2e27
Zsombor Gegesy <gzsombor@gmail.com>
2020-03-17 22:45:47 +0100

    More debug log in SubscriptionDb

--- app/src/main/java/free/rm/skytube/businessobjects/db/SubscriptionsDb.java
+++ app/src/main/java/free/rm/skytube/businessobjects/db/SubscriptionsDb.java
@@ -37,6 +37,7 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -445,6 +446,7 @@ public class SubscriptionsDb extends SQLiteOpenHelperEx {
                 SubscriptionsTable.COL_CHANNEL_ID + " = ?",
                 new String[]{channelId});
 
+        Logger.i(this, "Update last visit time on %s to %s -> %s", channelId, new Date(currentTime), count);
         return (count > 0 ? currentTime : -1);
     }
 
@@ -494,6 +496,9 @@ public class SubscriptionsDb extends SQLiteOpenHelperEx {
 				values,
 				SubscriptionsTable.COL_CHANNEL_ID + " = ?",
 				new String[]{channel.getId()});
+
+		Logger.i(this, "Update last visit time on %s to %s -> %s", channel.getId(), new Date(channel.getLastVisitTime()), count);
+
 		return count > 0;
 	}
 
@@ -826,6 +831,13 @@ public class SubscriptionsDb extends SQLiteOpenHelperEx {
 		return (rowsDeleted >= 0);
 	}
 
+	private String toDate(Long ts) {
+		if (ts != null) {
+			return new Date(ts.longValue()).toString();
+		} else {
+			return "<not set>";
+		}
+	}
 	public List<ChannelView> getSubscribedChannelsByText(String searchText, boolean sortChannelsAlphabetically) {
 		List<ChannelView> result = new ArrayList<>();
 		try (Cursor cursor = createSubscriptionCursor(searchText, sortChannelsAlphabetically)) {
