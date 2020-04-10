d809a55bc093e03d981f23ab823d739b7cec88b4
Zsombor Gegesy <gzsombor@gmail.com>
2020-04-11 01:59:49 +0200

    Remove noisy debug messages

--- app/src/main/java/free/rm/skytube/gui/fragments/YouTubePlayerV2Fragment.java
+++ app/src/main/java/free/rm/skytube/gui/fragments/YouTubePlayerV2Fragment.java
@@ -173,7 +173,7 @@ public class YouTubePlayerV2Fragment extends ImmersiveModeFragment implements Yo
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		hideNavigationBar();
-		Debug.logCaller(this, "onCreateView");
+		// Debug.logCaller(this, "onCreateView");
 
 
 		playerViewGestureHandler = new PlayerViewGestureHandler(SkyTubeApp.getSettings());
@@ -264,7 +264,7 @@ public class YouTubePlayerV2Fragment extends ImmersiveModeFragment implements Yo
 	 */
 	private void initViews(View view) {
 		// setup the toolbar / actionbar
-		Debug.logCaller(this, "initViews");
+		// Debug.logCaller(this, "initViews");
 
 		Toolbar toolbar = view.findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
@@ -701,7 +701,7 @@ public class YouTubePlayerV2Fragment extends ImmersiveModeFragment implements Yo
 
 	@Override
 	public void onDestroy() {
-		Debug.logCaller(this, "onDestroy");
+		// Debug.logCaller(this, "onDestroy");
 
 		compositeDisposable.clear();
 		super.onDestroy();
