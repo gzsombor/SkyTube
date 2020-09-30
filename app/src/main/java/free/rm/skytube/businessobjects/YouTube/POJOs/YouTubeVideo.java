4fea0cf1349013846e169a44b991dd8acc84b595
Zsombor Gegesy <gzsombor@gmail.com>
2020-10-01 01:54:30 +0200

    Categories - first steps

--- app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/YouTubeVideo.java
+++ app/src/main/java/free/rm/skytube/businessobjects/YouTube/POJOs/YouTubeVideo.java
@@ -128,6 +128,8 @@ public class YouTubeVideo extends CardData implements Serializable {
 	private Long retrievalTimestamp;
 
 
+	private Integer categoryId;
+
 	/**
 	 * Constructor.
 	 */
@@ -171,6 +173,14 @@ public class YouTubeVideo extends CardData implements Serializable {
 
 			setViewCount(statistics.getViewCount());
 		}
+	}
+
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Integer getCategoryId() {
+		return categoryId;
 	}
 
 	private void setViewCount(BigInteger viewsCountInt) {
