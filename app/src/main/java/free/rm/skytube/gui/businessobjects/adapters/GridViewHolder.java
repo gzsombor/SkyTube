195dad75eab0bccafed5da14d45933c09ca7451e
Zsombor Gegesy <gzsombor@gmail.com>
2020-10-05 00:36:16 +0200

    Fully enable the new subscribe button

--- app/src/main/java/free/rm/skytube/gui/businessobjects/adapters/GridViewHolder.java
+++ app/src/main/java/free/rm/skytube/gui/businessobjects/adapters/GridViewHolder.java
@@ -249,7 +249,7 @@ public class GridViewHolder extends RecyclerView.ViewHolder implements Serializa
 					SkyTubeApp.copyUrl(context, "Channel URL", channel.getChannelUrl());
 					return true;
 				case R.id.subscribe_channel:
-					YouTubeChannel.subscribeChannel(context, popupMenu.getMenu(), channel.getId());
+					YouTubeChannel.subscribeChannel(context, channel.getId());
 					return true;
 				case R.id.open_channel:
 					SkyTubeApp.launchChannel(channel.getId(), context);
@@ -338,7 +338,7 @@ public class GridViewHolder extends RecyclerView.ViewHolder implements Serializa
 					}
 					return true;
 				case R.id.subscribe_channel:
-					YouTubeChannel.subscribeChannel(context, popupMenu.getMenu(), youTubeVideo.getChannelId());
+					YouTubeChannel.subscribeChannel(context, youTubeVideo.getChannelId());
 					return true;
 				case R.id.open_channel:
 					SkyTubeApp.launchChannel(youTubeVideo.getChannelId(), context);
